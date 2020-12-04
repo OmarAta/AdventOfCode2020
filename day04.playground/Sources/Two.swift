05968484f9155469c1602eb275f8b8d5c674a92f
Omar Ata <oata@securustechnologies.com>
2020-12-04 11:50:48 -0600

    Day 4 solutions

---
+++ day04.playground/Sources/Two.swift
@@ -0,0 +1,83 @@
+import UIKit
+
+public class Two {
+    struct Height {
+        var value: Int
+        var unit: String
+    }
+    
+    struct Passport {
+        var valid = false
+        var byr: Int?
+        var iyr: Int?
+        var eyr: Int?
+        var hgt: Height?
+        var hcl: String?
+        var ecl: String?
+        var pid: String?
+        var cid: String?
+    }
+    
+    public init(_ input: String) {
+        var valid = 0
+        var passports = [Passport]()
+        let rows = input.components(separatedBy: "\n\n")
+        for row in rows {
+            let newPassport = parse(raw: row.replacingOccurrences(of: "\n", with: " "))
+            passports.append(newPassport)
+            valid += (newPassport.valid) ? 1 : 0
+        }
+        
+        print(valid)
+    }
+    
+    private func height(from: String?) -> Height? {
+        if let raw = from {
+            let value = Int(raw.components(separatedBy:CharacterSet.decimalDigits.inverted).joined()) ?? 0
+            let unit = raw.components(separatedBy:CharacterSet.decimalDigits).joined()
+            return Height(value: value, unit: unit)
+        } else {
+            return nil
+        }
+    }
+
+    private func parse(raw: String) -> Passport {
+        var passport = Passport()
+        let fields = raw.split(separator: " ")
+        for field in fields {
+            let split = field.split(separator: ":")
+            switch split[0] {
+            case "byr": passport.byr = Int(split[1])
+            case "iyr": passport.iyr = Int(split[1])
+            case "eyr": passport.eyr = Int(split[1])
+            case "hgt": passport.hgt = height(from: String(split[1]))
+            case "hcl": passport.hcl = String(split[1])
+            case "ecl": passport.ecl = String(split[1])
+            case "pid": passport.pid = String(split[1])
+            case "cid": passport.cid = String(split[1])
+            default: break
+            }
+        }
+        
+        if let byr = passport.byr,
+           let iyr = passport.iyr,
+           let eyr = passport.eyr,
+           let hgt = passport.hgt,
+           let hcl = passport.hcl,
+           let ecl = passport.ecl,
+           let pid = passport.pid {
+            if ((byr >= 1920 && byr <= 2002) &&
+                (iyr >= 2010 && iyr <= 2020) &&
+                (eyr >= 2020 && eyr <= 2030) &&
+                ((hgt.unit == "cm" && hgt.value >= 150 && hgt.value <= 193) || (hgt.unit == "in" && hgt.value >= 59 && hgt.value <= 76)) &&
+                (["amb","blu","brn","gry","grn","hzl","oth"].contains(ecl)) &&
+                (hcl.starts(with: "#") && hcl.filter(\.isHexDigit).count == 6 && hcl.count == 7) &&
+                (pid.count == 9 && Int(pid) != nil)
+            ) {
+                passport.valid = true
+            }
+        }
+        
+        return passport
+    }
+}
