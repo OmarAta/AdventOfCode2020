ac627fb0200ef6d228c0c3a122f2ac2b2029ac1b
Omar Ata <oata@securustechnologies.com>
2020-12-22 13:08:56 -0600

    Day 20 manual solution (2nd start)

--- day20.playground/Contents.swift
+++ day20.playground/Contents.swift
@@ -2,5 +2,10 @@ import UIKit
 
 let input = (try? String(contentsOf: Bundle.main.url(forResource: "input", withExtension: "")!)) ?? ""
 
-One(input)
-//Two(input)
+//One(input)
+Two(input)
+
+//15 per monster
+//2434 - 300 = 2134 -> too low
+//2434 - 150 = 2284 -> too high
+//2434 - 225 = 2209 -> Correct :D
